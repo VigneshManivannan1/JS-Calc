9fd4c4ed4d39dca91667fff20fdb454301315f44
VigneshManivannan1 <114759360+VigneshManivannan1@users.noreply.github.com>
2022-10-10 12:17:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS-Calc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-antjgc)
---
+++ index.js
@@ -0,0 +1,104 @@
+// Import stylesheets
+import './style.css';
+
+
+const calculator = {
+  displayValue: '0',
+  firstValue: null,
+  waitingForsecondValue: false,
+  operator: null,
+};
+
+
+
+function updateDisplay() {
+  const display=document.querySelector('.screen-calc');
+  display.value = calculator.displayValue;
+}
+
+function resetCalculator() {
+  calculator.displayValue = '0';
+  calculator.firstValue = null;
+  calculator.waitingForsecondValue = false;
+  calculator.operator = null;
+}
+
+updateDisplay();
+
+const keys= document.querySelector('.btn-calc');
+keys.addEventListener('click', (event) => {
+  const target = event.target;
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    handleOperator(target.value);
+    updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    inputDecimal(target.value);
+    updateDisplay();
+    return;
+  }
+
+  if (target.classList.contains('clear')) {
+    resetCalculator();
+    updateDisplay();
+  }
+
+  inputDigit(target.value);
+  updateDisplay();
+  return;
+});
+
+function inputDigit(digit) {
+  const { displayValue, waitingForsecondValue } = calculator;
+
+  if (waitingForsecondValue === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForsecondValue = false;
+  } else {
+    calculator.displayValue =
+      displayValue === '0' ? digit : displayValue + digit;
+  }
+}
+
+function inputDecimal(dot) {
+  if (calculator.waitingForsecondValue === true) {
+    return;
+  }
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot;
+  }
+}
+const performCalculation = {
+  '/': (firstValue, secondValue) => firstValue / secondValue,
+  '*': (firstValue, secondValue) => firstValue * secondValue,
+  '+': (firstValue, secondValue) => firstValue + secondValue,
+  '-': (firstValue, secondValue) => firstValue - secondValue,
+  '=': (firstValue, secondValue) => secondValue,
+};
+
+function handleOperator(nextOperator) {
+  const { firstValue, displayValue, operator } = calculator;
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && calculator.waitingForsecondValue) {
+    calculator.operator = nextOperator;
+    return;
+  }
+
+  if (firstValue == null) {
+    calculator.firstValue = inputValue;
+  } else if (operator) {
+    const currentValue = firstValue || 0;
+    const result = performCalculation[operator](currentValue, inputValue);
+    calculator.displayValue = String(result);
+    calculator.firstValue = result;
+  }
+  calculator.waitingForsecondValue = true;
+  calculator.operator = nextOperator;
+}
